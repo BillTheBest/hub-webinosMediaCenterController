7eb78f849f21de8dcc8980cd8a1bf24e8bc4dbfc
Paolo Vergori <vergori@ismb.it>
2013-07-04 16:18:55 +0200

    fileMenu added and dinamically populated

--- js/dropDown.js
+++ js/dropDown.js
@@ -1,8 +1,5 @@
-function enableSelectBoxes(){
-    $('div.serviceBox').each(function(){
-        $(this).children('span.selected').html($(this).children('ul.selectOptions').children('li.selectOption:first').html());
-        $('input.price_values').attr('value',$(this).children('ul.selectOptions').children('li.selectOption:first').attr('data-value'));
-        
+function enableServiceBoxes(){
+    $('div.serviceBox').each(function(){        
         $(this).children('span.selected,span.selectArrow').click(function(){
             if($(this).parent().children('ul.selectOptions').css('display') == 'none'){
                 $(this).parent().children('ul.selectOptions').css('display','block');
@@ -13,14 +10,35 @@ function enableSelectBoxes(){
             }
         });
                 
-                $(this).find('li.selectOption').click(function(){
-                    $(this).parent().css('display','none');
-                    $('input.price_values').attr('value',$(this).attr('data-value'));
-                    $(this).parent().siblings('span.selected').html($(this).html());
-                });
+        $(this).find('li.selectOption').click(function(){
+            $(this).parent().css('display','none');
+            $(this).parent().siblings('span.selected').html($(this).html());
+        });
     });             
 } 
 
+
+function enableFilesBoxes(){
+    $('div.filesBox').each(function(){      
+        $(this).children('span.selected_file,span.selectArrow_file').click(function(){
+            if($(this).parent().children('ul.selectOptions_file').css('display') == 'none'){
+                $(this).parent().children('ul.selectOptions_file').css('display','block');
+            }
+            else
+            {
+                $(this).parent().children('ul.selectOptions_file').css('display','none');
+            }
+        });
+        
+        $(this).find('li.selectOption_file').click(function(){
+            $(this).parent().css('display','none');
+            $(this).parent().siblings('span.selected_file').html($(this).html());
+        });
+    });             
+} 
+
+
 $(document).ready(function() {
-    enableSelectBoxes();
+    enableServiceBoxes();
+    enableFilesBoxes();
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+function addService(displayName){
+     $('ul.selectOptions').append('<li class="selectOption">' + displayName + '</li>');    
+}
  
- $(document).ready(function() {
+function addFile(displayName){
+     $('ul.selectOptions_file').append('<li class="selectOption_file">' + displayName + '</li>');     
+     $('ul.selectOptions_file').css('top', $('ul.selectOptions_file').css('top').replace('px','')-30);     
+}
+ 
+ 
 
- });
+$(document).ready(function() {
+     addService("Media-API #1");
+     addService("Media-API #2");
+     addService("Media-API #3");
+     addService("Media-API #4");
+     addService("Media-API #5");
+     addFile("File #1");
+     addFile("File #2");
+     addFile("File #3");
+     addFile("File #4");
+});
