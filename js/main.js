23c6fcea80227a4aa239ff05bcd25b0da12120bc
Paolo Vergori <vergori@ismb.it>
2013-10-11 16:17:42 +0200

    added xbmc implementation support and dropdown autorefresh Jira-issue HUBWMC-32

--- js/main.js
+++ js/main.js
@@ -20,9 +20,15 @@ function addFile(displayName){
 function fillServicesIn(){
     webinos.discovery.findServices(new ServiceType('http://webinos.org/api/media'), {                
         onFound: function(service){            
-            var serviceID = service.serviceAddress.replace(/[^a-z0-9]/gi, '_');
+//             var serviceID = service.serviceAddress.replace(/[^a-z0-9]/gi, '_');
+            var serviceID = service.id;
+//             console.debug(service);
+//             console.debug(webinos.session);
+//             console.debug(webinos.session.getPZPId());
+//             console.debug(webinos.session.getConnectedPzp());
+//             console.debug(webinos.session.getConnectedDevices());
             
-            $('ul.selectOptions').append('<li id="' + serviceID + '" class="selectOption" data="' + service + '">' + service.serviceAddress + '</li>');                                   
+            $('ul.selectOptions').append('<li id="' + serviceID + '" class="selectOption" data="' + service + '">' + service.serviceAddress + ' ' + service.displayName + '</li>');                                   
             $('#'+serviceID).click(function(){
                 $(this).parent().css('display','none');
                 $(this).parent().siblings('span.selected').html($(this).html());             
@@ -59,25 +65,108 @@ function fillServicesIn(){
                             }
                         }, errorCB);
                         
-                        webinos.discovery.findServices(new ServiceType('http://webinos.org/api/file'), {                
-                            onFound: function(fileService){                                            
-                                
-                                if(fileService.serviceAddress === service.serviceAddress){
-                                    fileService.bindService({
-                                        onBind: function(fileService){
-                                            console.debug("***Bound TO FILE***");
-                                            console.debug(fileService);
-                                            console.debug("*******************");                                                             
-                                            $('ul.selectOptions_file').empty();
-                                            fileService.requestFileSystem(1, 1024,function(fileSystem){  
-                                                mediaService.exportedPath = fileService.description.replace(fileSystem.name+': ' ,'')+'/';
-                                                loadDirectory(fileSystem.root);
-                                            },function(){console.debug("errorCB");});
-                                        }
-                                    });
+                        if(service.description.indexOf('(XBMC)') == -1){
+                            webinos.discovery.findServices(new ServiceType('http://webinos.org/api/file'), {                
+                                onFound: function(fileService){                                            
+                                    
+                                    if(fileService.serviceAddress === service.serviceAddress){
+                                        fileService.bindService({
+                                            onBind: function(fileService){
+                                                console.debug("***Bound TO FILE***");
+                                                console.debug(fileService);
+                                                console.debug("*******************");                                                             
+                                                $('ul.selectOptions_file').empty();
+                                                fileService.requestFileSystem(1, 1024,function(fileSystem){  
+                                                    mediaService.exportedPath = fileService.description.replace(fileSystem.name+': ' ,'')+'/';
+                                                    loadDirectory(fileSystem.root);
+                                                },function(){console.debug("errorCB");});
+                                            }
+                                        });
+                                    }
                                 }
+                            });
+                        }
+                        else{ //binding to XBMC                            
+                            var xbmcIpAddr = service.description.replace(/.*\ /gi, '');
+//                             var url = 'http://xbmc:@'+xbmcIpAddr+':8080/jsonrpc', _data = {jsonrpc:"2.0", method:"JSONRPC.Version", id:"13"};                            
+//                              $.ajaxSetup({converters:{'jsonp':function(raw){console.debug(raw);return raw;}}});
+//                              $.ajax({
+//                                     url: url+'?request='+JSON.stringify(_data),
+//                                     type: 'GET',
+//                                     dataType: 'jsonp',
+//                                     dataFilter: function(data, dataType){console.debug(data);console.debug(dataType);};,
+//                                     jsonp: 'function(){}',
+//                                     converters: {"jsonp":function(data){console.debug(data);return data;}},
+//                                     contentType: 'application/json',
+//                                     success: function(data) {
+//                                         console.debug("--------------json2rpc success"); 
+//                                         console.debug(data); 
+//                                     },
+//                                     error: function(xhr, status, err) {
+//                                         console.error("----------------json2rpc error: " + status);
+//                                         console.debug(xhr);
+//                                         console.debug(status);
+//                                         console.debug(err);
+//                                     }
+//                              });
+            
+                            if(!'WebSocket' in window){
+                                 console.error("browser does not supports websokets");
+                                 return false;
                             }
-                        });
+                            else
+                                console.debug("browser supports websokets");
+                            
+                            var wsUrl = unescape(encodeURI('ws://'+xbmcIpAddr+':9090/jsonrpc')),
+                                ws = new WebSocket(wsUrl),
+                                id = 100,
+                                _data = {id:++id, jsonrpc:'2.0', method:'Files.GetDirectory', params:{directory:'/sdcard/Movies', media:'video'}};
+                                
+                            $('ul.selectOptions_file').empty();
+                            $('ul.selectOptions_file').append('<li id="-1" class="selectOption_file" data="">Now loading...</li>');
+                            
+                            var timerId = setTimeout(function(){
+                                delete ws;
+                                $('ul.selectOptions_file').empty();
+                                $('ul.selectOptions_file').append('<li id="-1" class="selectOption_file" data="">Connection hanged. Please reselect the source device.</li>');
+                            }, 10000);
+
+                            ws.onopen = function(){
+                                 console.debug("ws opened");
+                                 ws.send(JSON.stringify(_data));
+                            };
+                            ws.onmessage = function(evt){
+                                var data = JSON.parse(evt.data);
+                                if(data.id !== undefined){
+                                    console.debug('----onMessage---');
+                                    console.debug(data);
+                                    console.debug(data.result.files);
+                                    console.debug('----------------');
+                                    $('ul.selectOptions_file').empty();
+
+                                    if(data.result.files.length>0)
+                                        mediaService.exportedPath = data.result.files[0].file.replace(data.result.files[0].label, '');
+                                    for(var i=0;i<data.result.files.length;i++)
+                                        if(data.result.files[i].filetype === 'file')
+                                            addFile(data.result.files[i].label);
+                                }
+                                if(data.id == id){
+                                    delete ws;
+                                    clearTimeout(timerId);
+                                }
+                            };
+                            
+                            ws.onerror = function(err){
+                                console.debug('----onError---');
+                                console.error(err);
+                                $('ul.selectOptions_file').empty();
+                                $('ul.selectOptions_file').append('<li id="-1" class="selectOption_file" data="">Error while loading files...</li>');
+                                console.debug('--------------');
+                            };
+                            ws.onclose = function(){
+                                console.debug("ws is closed.");
+                            };
+                        }
                     }
                 });
             });
@@ -86,6 +175,7 @@ function fillServicesIn(){
 }
 
 
+function callback(data, dataType){console.debug("TUA MAMMA"); console.debug(data); console.debug(dataType);};
 
 $(document).ready(function() {
     webinos.session.addListener('registeredBrowser', fillServicesIn);
@@ -97,7 +187,6 @@ $(document).ready(function() {
             mediaService.selected.unregisterListenersOnExit();
     });
 });
-
 
 
 function loadDirectory(directory){
